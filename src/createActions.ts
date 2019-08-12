c8247d1e5faa00ea6e392e936ba460a2f47566b5
Ariel Flor <aflor@thoughtworks.com>
2019-08-12 10:50:38 -0300

    add typings more types

--- src/createActions.ts
+++ src/createActions.ts
@@ -4,11 +4,41 @@ import createCreators from './createCreators';
 import validateConfig from './validateConfig';
 import toExternalTypes from './toExternalTypes';
 
+export enum HTTPMethod {
+  connect = 'connect',
+  delete = 'delete',
+  get = 'get',
+  head = 'head',
+  options = 'options',
+  patch = 'patch',
+  post = 'post',
+  put = 'put',
+  trace = 'trace'
+}
+
+export enum ApplyPoint {
+  onResponse = 'onResponse',
+  onRequest = 'onRequest'
+}
+
+export interface AsyncActionConfig {
+  url: string,
+  method: HTTPMethod,
+  middlewares: Function[]
+}
+
+type ActionConfigValue = null | string | number | AsyncActionConfig
+
+export interface ActionsConfiguration {
+  [key: string]: ActionConfigValue
+}
+
 /*
   @param {string} namespace - namespace to be uppercased and prefix your action types
   @param {Array} config - object with options
 */
-export default function createActions(namespace, config) {
+
+export default function createActions(namespace: string, config: ActionsConfiguration) {
   const NAMESPACE = parseToUppercase(namespace);
   validateConfig(NAMESPACE, config);
 
